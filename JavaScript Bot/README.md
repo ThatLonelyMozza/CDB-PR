a4590620988c2efd91ab0ee3dd506685bc75b32d
TheLonelyForth <owouwueue@gmail.com>
2020-07-28 20:14:41 -0400

    .

---
+++ JavaScript Bot/README.md
@@ -0,0 +1,11 @@
+# JavaScript Version
+Originally Started by Fiire
+
+# Contributors - JSV
+@TheLonelyForth
+
+
+###### (Add your name whenever you make changes to these Files)
+
+### Version History
+v1 - Initial Commit <3
--- JavaScript Bot/index.js
+++ JavaScript Bot/index.js
@@ -2,6 +2,7 @@
     let Discord = require('discord.js')
     let Client = new (require('./classes/client'))()
 
+    // Event Handler
     for(let event of require('fs').readdirSync('./events/')) {
         let eventFile = require('./events/'+event)
         let name = eventFile.name || event.split(".")[0]
@@ -10,5 +11,6 @@
         Client[once ? "once" : "on"](name, (...args) => eventFile.run(Client, ...args))
     }
 
+    // Login using Token
     Client.login(Client.config["keys"].discord_token)
 })
